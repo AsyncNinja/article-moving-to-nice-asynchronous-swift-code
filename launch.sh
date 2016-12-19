d36b9b25f1b0c843f211b5126188f950e99d0c46
Anton Mironov <antonvmironov@gmail.com>
2016-12-19 16:40:47 +0200

    Fixing launch.sh to work with paths that contain spaces

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 swift build
-PERSONS_JSON=$PWD/Resources/persons.json
+PERSONS_JSON="$PWD/Resources/persons.json"
 
-"./.build/debug/Step 0 - Sync" $PERSONS_JSON
-"./.build/debug/Step 1.0 - Callbacks" $PERSONS_JSON
-"./.build/debug/Step 1.1 - Callbacks Full Story" $PERSONS_JSON
-"./.build/debug/Step 2.0 - Futures" $PERSONS_JSON
-"./.build/debug/Step 2.1 - Futures Full Story" $PERSONS_JSON
-"./.build/debug/Step 2.2 - Futures and ExecutionContext" $PERSONS_JSON
+"./.build/debug/Step 0 - Sync" "$PERSONS_JSON"
+"./.build/debug/Step 1.0 - Callbacks" "$PERSONS_JSON"
+"./.build/debug/Step 1.1 - Callbacks Full Story" "$PERSONS_JSON"
+"./.build/debug/Step 2.0 - Futures" "$PERSONS_JSON"
+"./.build/debug/Step 2.1 - Futures Full Story" "$PERSONS_JSON"
+"./.build/debug/Step 2.2 - Futures and ExecutionContext" "$PERSONS_JSON"
